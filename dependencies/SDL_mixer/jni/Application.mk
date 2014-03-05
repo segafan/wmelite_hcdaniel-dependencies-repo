102fb3f39600a7f479a86a9b2e377fcf0bdfbbcf
hardcoredaniel <hardcoredaniel@localhost>
2014-03-05 11:12:20 +0100

    added SDL2_mixer library (experimental)

---
+++ dependencies/SDL_mixer/jni/Application.mk
@@ -0,0 +1,11 @@
+
+# Uncomment this if you're using STL in your project
+# See CPLUSPLUS-SUPPORT.html in the NDK documentation for more information
+# APP_STL := stlport_static 
+
+APP_OPTIM := release
+
+APP_PLATFORM := android-9
+
+APP_ABI := armeabi armeabi-v7a
+
