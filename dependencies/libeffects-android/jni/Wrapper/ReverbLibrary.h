ba22588e09ef0e8c051cd8af1ffaddf7d9b765b3
hardcoredaniel <hardcoredaniel@localhost>
2014-12-10 11:10:29 +0100

    stdint removed as well

--- dependencies/libeffects-android/jni/Wrapper/ReverbLibrary.h
+++ dependencies/libeffects-android/jni/Wrapper/ReverbLibrary.h
@@ -20,7 +20,15 @@
 // from Reverb/lib
 #include "LVREV.h"
 
-#include <stdint.h>
+// #include <stdint.h>
+
+typedef signed char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int uint32_t;
 
 #include <audio_effects/effect_environmentalreverb.h>
 #include <audio_effects/effect_presetreverb.h>
