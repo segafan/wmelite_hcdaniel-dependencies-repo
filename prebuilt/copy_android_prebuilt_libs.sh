39ef903f4ecda69560375c293ee88061fd7981c4
hardcoredaniel <hardcoredaniel@localhost>
2014-12-11 20:45:08 +0100

    copy libeffects as well

--- prebuilt/copy_android_prebuilt_libs.sh
+++ prebuilt/copy_android_prebuilt_libs.sh
@@ -4,14 +4,17 @@ cp ../dependencies/FreeImage3154/libs/armeabi/libfreeimage.so android/armeabi/
 cp ../dependencies/libfreetype-android/obj/local/armeabi/libfreetype.a android/armeabi/
 cp ../dependencies/SDL/libs/armeabi/libSDL2.so android/armeabi/
 cp ../dependencies/SDL_mixer/libs/armeabi/libSDL2_mixer.so android/armeabi/
+cp ../dependencies/libeffects-android/libs/armeabi/libeffects.so android/armeabi/
 
 cp ../dependencies/FreeImage3154/libs/armeabi-v7a/libfreeimage.so android/armeabi-v7a/
 cp ../dependencies/libfreetype-android/obj/local/armeabi-v7a/libfreetype.a android/armeabi-v7a/
 cp ../dependencies/SDL/libs/armeabi-v7a/libSDL2.so android/armeabi-v7a/
 cp ../dependencies/SDL_mixer/libs/armeabi-v7a/libSDL2_mixer.so android/armeabi-v7a/
+cp ../dependencies/libeffects-android/libs/armeabi-v7a/libeffects.so android/armeabi-v7a/
 
 cp ../dependencies/FreeImage3154/libs/x86/libfreeimage.so android/x86/
 cp ../dependencies/libfreetype-android/obj/local/x86/libfreetype.a android/x86/
 cp ../dependencies/SDL/libs/x86/libSDL2.so android/x86/
 cp ../dependencies/SDL_mixer/libs/x86/libSDL2_mixer.so android/x86/
+cp ../dependencies/libeffects-android/libs/x86/libeffects.so android/x86/
 
