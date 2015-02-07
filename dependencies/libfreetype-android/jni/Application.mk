dacd4d74ca8b232a26ea6d32bfcebd5376083f08
hcdaniel <hcdaniel@localhost>
2015-02-07 10:41:05 +0100

    disable hard float again

--- dependencies/libfreetype-android/jni/Application.mk
+++ dependencies/libfreetype-android/jni/Application.mk
@@ -3,7 +3,7 @@ APP_PLATFORM := android-9
 
 APP_OPTIM := release
 
-APP_ABI := armeabi armeabi-v7a-hard x86 mips arm64-v8a x86_64 mips64
+APP_ABI := armeabi armeabi-v7a x86 mips arm64-v8a x86_64 mips64
 
 APP_MODULES := libfreetype
 
