74679b4d423639bd12545ad573e487b935375b30
hcdaniel <hcdaniel@localhost>
2015-03-28 12:20:02 +0100

    add hint for UI_FLAG_LOW_PROFILE on Android

--- dependencies/SDL/jni/include/SDL_hints.h
+++ dependencies/SDL/jni/include/SDL_hints.h
@@ -521,6 +521,11 @@ extern "C" {
 #define SDL_HINT_ANDROID_APK_EXPANSION_PATCH_FILE_VERSION "SDL_ANDROID_APK_EXPANSION_PATCH_FILE_VERSION"
 
 /**
+ * \brief If set to 1, sets UI_LOW_PROFILE setting for SDLActivity
+ */
+#define SDL_HINT_ANDROID_USE_UI_LOW_PROFILE "SDL_ANDROID_USE_UI_LOW_PROFILE"
+
+/**
  * \brief A variable to control whether certain IMEs should handle text editing internally instead of sending SDL_TEXTEDITING events.
  *
  * The variable can be set to the following values:
